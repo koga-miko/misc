fb6c0b22e096fa8aabce90610303dd9bfb3efcd2
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2023-12-10 10:15:47 +0900

    create main.c for entry point

---
+++ build-script/build_script.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subproess.run(["docker", "exec", "-it", "jupyter", "."])
+#todo: 入出力は、バージョン、ベースのもととなるリポジトリ、を指定する。
+
+#todo: docker run -dit --name my-running-script -v "$PWD"/work:/home/hoge/work docker_image_name
+#todo: docker
