30b15c2b013f5d93de26e3975c338e43439d7bba
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2024-07-19 12:44:07 +0900

    modify for describing of gRPC error code

--- grpc_test/server.py
+++ grpc_test/server.py
@@ -7,7 +7,28 @@ class UsageService(usage_pb2_grpc.UsageServiceServicer):
     def GetUsage(self, request, context):
         # ここではサンプルとして固定値を返します
         data_usage = 123456789
-        return usage_pb2.UsageResponse(data_usage=data_usage)
+
+        # サーバー側から応答可能な様々なエラー応答の分岐を作成する
+        if data_usage < 0:
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details('Usage is invalid')
+            return usage_pb2.UsageResponse()
+        elif data_usage > 100000000:
+            context.set_code(grpc.StatusCode.RESOURCE_EXHAUSTED)
+            context.set_details('Usage is too large')
+            return usage_pb2.UsageResponse()
+        elif data_usage == 0:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details('Usage is not found')
+            return usage_pb2.UsageResponse()
+        elif data_usage == 123456789:
+            context.set_code(grpc.StatusCode.OK)
+            context.set_details('Usage is found')
+            return usage_pb2.UsageResponse(data_usage=data_usage)
+        else:
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details('Unknown error')
+            return usage_pb2.UsageResponse()
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
