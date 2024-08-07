1178b412b30f5ec6f0d4e0f1daab1c41498f84df
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2024-08-07 22:52:45 +0900

    create copipe-exec as VS-Code extension

--- copipe-exec/src/extension.ts
+++ copipe-exec/src/extension.ts
@@ -8,15 +8,41 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copipe" is now active!');
+	console.log('Congratulations, your extension "copipe-exec" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('copipe.execute', () => {
+	const disposable = vscode.commands.registerCommand('copipe-exec.excecute', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from copipe!');
+		vscode.window.showInformationMessage('Hello World from copipe-exec!');
+
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+
+		// If there is no active text editor, then return
+		if (!editor) {
+			return;
+		}
+		// Get the document object from the active text editor
+		const document = editor.document;
+
+		// Get the selection object from the active text editor
+		const selection = editor.selection;
+
+		// Get the text from the selection object
+		const text = document.getText(selection);
+
+		// Get the terminal
+		const terminal = vscode.window.createTerminal('Copipe Exec');
+
+		// Run the text in the terminal
+		terminal.sendText(text);
+
+		// Show the terminal
+		terminal.show();
+
 	});
 
 	context.subscriptions.push(disposable);
