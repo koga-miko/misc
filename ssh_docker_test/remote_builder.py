09e468684fcd7d47c9842c981cc4cba7e65fe827
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2024-10-08 09:04:31 +0900

    modify ssh_docker_test

--- ssh_docker_test/remote_builder.py
+++ ssh_docker_test/remote_builder.py
@@ -4,10 +4,16 @@
 import paramiko
 import time
 import yaml
+import re
 
 class RemoteBuilder:
     def __init__(self, build_config_yaml):
-        self.set_build_config(build_config_yaml)
+        self.load_build_config(build_config_yaml)
+        self.enviroment = {}
+        # yamlファイルからリージョン情報を取得
+
+    def get_region_list(self):
+        return self.region_list;
 
     def exec_build(self, build_script_yaml, exec_result_listener):
         self.ssh = paramiko.SSHClient()
@@ -17,33 +23,36 @@ class RemoteBuilder:
     def cancel_build(self, cancel_result_listener):
         pass
 
-    def set_build_config(self, build_config_yaml):
-        with open(build_config_yaml, 'r') as file:
-            self.build_config = yaml.safe_load(file)
-
     def close(self):
         self.ssh.close()
 
-def _send_commands_and_wait_until_done(self, commands, done_reply_message, recv_size_max=1024, timeout=10, sleep_time=1):
-    channel = self.ssh.invoke_shell()
-    for command in commands:
-        # インタラクティブなシェルセッションを開始
-        print(f"Sending command: {command}")
-        channel.send(command + '\n')
-    time.sleep(sleep_time)
-    start_time = time.time()
-    while True:
-        if channel.recv_ready():
-            output = channel.recv(recv_size_max).decode('utf-8')
-            print(f"Received output: {output}", end='')
-            if done_reply_message in output:
-                print(f"Received done message: {done_reply_message}. Exiting.")
-                break
-        if time.time() - start_time > timeout:
-            print(f"Timeout reached. Exiting.")
-            break
+    def load_build_config(self, build_config_yaml):
+        with open(build_config_yaml, 'r') as file:
+            self.build_config = yaml.safe_load(file)
+
+    def get_region_info(self, region):
+        return self.build_config['region_info']
+
+    def _send_commands_and_wait_until_done(self, commands, done_reply_message, recv_size_max=1024, timeout=10, sleep_time=1):
+        channel = self.ssh.invoke_shell()
+        for command in commands:
+            # インタラクティブなシェルセッションを開始
+            print(f"Sending command: {command}")
+            channel.send(command + '\n')
         time.sleep(sleep_time)
-    channel.close()
+        start_time = time.time()
+        while True:
+            if channel.recv_ready():
+                output = channel.recv(recv_size_max).decode('utf-8')
+                print(f"Received output: {output}", end='')
+                if done_reply_message in output:
+                    print(f"Received done message: {done_reply_message}. Exiting.")
+                    break
+            if time.time() - start_time > timeout:
+                print(f"Timeout reached. Exiting.")
+                break
+            time.sleep(sleep_time)
+        channel.close()
 
 if __name__ == '__main__':
     # テスト用
--- ssh_docker_test/remote_builder_gui.py
+++ ssh_docker_test/remote_builder_gui.py
@@ -7,41 +7,37 @@ import time
 from remote_builder import RemoteBuilder
 class RemoteBuilderGUI:
     def __init__(self):
-        self.remote_builder = RemoteBuilder("./build_config.yaml")
+        self.rmt_bldr = RemoteBuilder("./build_config.yaml")
         self.root = tk.Tk()
         self.root.title("ビルド管理ツール")
         self.root.geometry("600x400")
         self.create_widgets()
     
     def create_widgets(self):
-        tk.Label(self.root, text="ビルド用設定ファイルのパス:").grid(row=0, column=0, sticky=tk.W, padx=10, pady=5)
-        self.config_path = tk.Entry(self.root, width=50)
-        self.config_path.grid(row=0, column=1, padx=10, pady=5)
-
         tk.Label(self.root, text="リリースバージョン:").grid(row=1, column=0, sticky=tk.W, padx=10, pady=5)
         self.release_version = tk.Entry(self.root, width=50)
-        self.release_version.grid(row=1, column=1, padx=10, pady=5)
+        self.release_version.grid(row=0, column=1, padx=10, pady=5)
 
         tk.Label(self.root, text="baselineのブランチ名:").grid(row=2, column=0, sticky=tk.W, padx=10, pady=5)
         self.branch_name = tk.Entry(self.root, width=50)
-        self.branch_name.grid(row=2, column=1, padx=10, pady=5)
+        self.branch_name.grid(row=1, column=1, padx=10, pady=5)
 
         tk.Label(self.root, text="仕向け品番:").grid(row=3, column=0, sticky=tk.W, padx=10, pady=5)
-        self.product_list = ttk.Combobox(self.root, values=["品番1", "品番2"], width=47)
-        self.product_list.grid(row=3, column=1, padx=10, pady=5)
+        self.product_list = ttk.Combobox(self.root, values=[region['name'] for region in self.rmt_bldr['regions']], width=47)
+        self.product_list.grid(row=2, column=1, padx=10, pady=5)
 
         tk.Label(self.root, text="ビルドの実行ステータス:").grid(row=7, column=0, sticky=tk.W, padx=10, pady=5)
         self.status_text = scrolledtext.ScrolledText(self.root, width=50, height=10)
-        self.status_text.grid(row=4, column=1, padx=10, pady=5)
+        self.status_text.grid(row=3, column=1, padx=10, pady=5)
         
         self.run_button = tk.Button(self.root, text="ビルド準備", command=self.prepare_build)
-        self.run_button.grid(row=5, column=0, padx=10, pady=10)
+        self.run_button.grid(row=4, column=0, padx=10, pady=10)
 
         self.run_button = tk.Button(self.root, text="ビルド実行", command=self.run_build)
-        self.run_button.grid(row=5, column=1, padx=10, pady=10)
+        self.run_button.grid(row=4, column=1, padx=10, pady=10)
 
         self.cancel_button = tk.Button(self.root, text="ビルドキャンセル", command=self.cancel_build)
-        self.cancel_button.grid(row=5, column=2, padx=10, pady=10)
+        self.cancel_button.grid(row=4, column=2, padx=10, pady=10)
 
     def run_build(self):
         # 各TextBoxや選択リストの情報を取得
